f47dfd63272c20982f62694a149dcfdcb9793135
Marcos Lima <96293424+marcoslima42@users.noreply.github.com>
2023-09-18 15:21:11 -0300

    Update q1.c

--- trabalho1/q1.c
+++ trabalho1/q1.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
-#include <conio.h>
+
 
 int main(){
-    float salario=0;
-    int dp=0, cont=1;
-    printf("\nRELATORIO DE FUNCIONARIOS\n");
-    while(cont<=4){
-    //entrada
-        printf("\nFUNCIONARIO %d\n", cont);
-        printf("\nDigite o salario do funcionario %d: ", cont);
-        scanf("%f", &salario);
-        printf("\nDigite a quantidade de dependentes: ");
-        scanf("%d", &dp);
-        system("cls");
-        printf("\nRELATORIO FUNCIONARIO %d\n\n", cont);
-        if(dp==1)
-            salario*=1.02;
-        else if(dp==2)
-            salario*=1.04;
-        else if(dp==3)
-            salario*=1.06;
-        else if(dp==4)
-            salario*=1.08;
-        else
-            salario*=1.1;
-        printf("O Salario liquido e de %.2f", salario);
+    float real=0, maior=0, menor=0;
+    int cont=1, primeiraVez=1;
+
+    printf("\nINSIRA 15 NUMEROS REAIS E DESCUBRA O MAIOR E MENOR\n");
+    for(int i=1; i<=15; i++){
+        printf("\nNumero %d: ", cont);
         cont++;
-        getch();
-        system("cls");
-}
+        scanf("%f", &real);
+    
+    if(primeiraVez==1){
+        maior=menor=real;
+        primeiraVez=0;
+    }
+
+    if(real>maior)
+        maior=real;
+    if(real<menor)
+        menor=real;
+    }
+
+
+    printf("\nO maior numero e o %.2f", maior);
+    printf("\nO menor numero e o %.2f", menor);
+
 
     return 0;
 }
