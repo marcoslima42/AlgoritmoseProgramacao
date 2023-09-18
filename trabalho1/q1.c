6eb02ca7bf459b0b784ead9adcfbfd3907a7ac9a
Marcos Lima <96293424+marcoslima42@users.noreply.github.com>
2023-09-18 11:55:20 -0300

    add q1 e q2

--- trabalho1/q1.c
+++ trabalho1/q1.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
 int main(){
-    float real=0, maior=0, menor=0;
-    int cont=1;
+    float real=0, maior=0, menor=1;
+    int cont=1, primeiraVez=1;
 
-    printf("\nINSIRA 15 NUMEROS REAIS E DESCUBRA O MAIOR E MENOR\n\n");
-    for(int i=0; i<=15; i++){
-        printf("\n\nNumero %d: ", cont);
+    printf("\nINSIRA 15 NUMEROS REAIS E DESCUBRA O MAIOR E MENOR\n");
+    for(int i=1; i<=3; i++){
+        printf("\nNumero %d: ", cont);
         cont++;
         scanf("%f", &real);
+        if(primeiraVez==1){
+            maior=menor=real;
+            primeiraVez=0;
+        }
+
         if(real>maior)
             maior=real;
         if(real<menor)
             menor=real;
     }
 
-    printf("\n\nO maior numero e o %.2f", real);
-    printf("\n\nO menor numero e o %.2f", real);
-
+    printf("\nO maior numero e o %.2f", maior);
+    printf("\nO menor numero e o %.2f", menor);
+    
     return 0;    
 }
---
+++ trabalho1/q2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+    float salario=0;
+    int dp=0, cont=1;
+
+    printf("\nRELATORIO DE FUNCIONARIOS\n");
+
+    while(cont<=4){
+        //entrada
+        printf("\nFUNCIONARIO %d\n", cont);
+        printf("\nDigite o salario do funcionario %d: ", cont);
+        scanf("%f", &salario);
+        printf("\nDigite a quantidade de dependentes: ");
+        scanf("%d", &dp);
+
+        //saida
+        //system(cls);
+        printf("\nRELATORIO FUNCIONARIO %d\n\n", cont);
+        if(dp==1)
+            salario*=1.02;
+        else if(dp==2)
+            salario*=1.04;
+        else if(dp==3)
+            salario*=1.06;
+        else if(dp==4)
+            salario*=1.08;
+        else
+            salario*=1.1;
+        printf("O Salario liquido e de %.2f", salario);
+        cont++;
+        //system(cls);
+
+    }
+    
+    return 0;
+}
+
