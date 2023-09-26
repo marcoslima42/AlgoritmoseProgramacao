409a215b2c6eb66eb4e5041d7a6f6dd0819cc887
Marcos Lima <araujolima728@gmail.com>
2023-09-25 21:51:25 -0300

    alteracao na q3

--- trabalho1/q3_falha.c
+++ trabalho1/q3_falha.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 
 int main(){
-    float montante=0, capital=0, taxa=0, juros=0;
     int tempo=0;
     char opc='S';
 
     while(opc=='S' || opc=='s'){    
-        montante=0, capital=0, taxa=0, juros=0;
+        int primeiroMes=1;
+        float montante=0, capital=0, taxa=0, juros=0, aux=0;
         
         //entrada
         printf("\nJUROS COMPOSTO");
         printf("\nInsira o capital ao mes: ");
         scanf("%f", &capital);
+        aux=capital;
         printf("\nInsira a taxa de juros: ");
         scanf("%f", &taxa);
         
         //processamento 
-        for(tempo=1; tempo<=11; tempo++){
-            juros=capital*(taxa/100);
-            montante=montante+(capital+juros);
-            capital=capital+juros;
+        for(tempo=1; tempo<=12; tempo++){
+            if(primeiroMes<12){
+                juros=capital*(taxa/100);
+                montante=montante+(capital+juros);
+                capital=capital+juros;
+                primeiroMes++;
+            }
+            else 
+            montante+=aux;
         }
+        
 
         //saida
         printf("\n\n\nSaldo do investimento apos um ano: ");
-        printf("\nR$ %.2f", montante+100);
+        printf("\nR$ %.2f", montante);
         printf("\n\nDESEJA EXECUTAR NOVAMENTE?");
         printf("\nSE SIM DIGITE 'S', SE NAO INSIRA QUALQUER TECLA\n");
         opc=getch();
