faffa5302999aba4534d4360e81698348ddd23fb
Marcos Lima <96293424+marcoslima42@users.noreply.github.com>
2023-10-15 11:28:59 -0300

    add test compiler

---
+++ testeCompilador/compilando.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+void menu(){
+    printf("\n\nmenu");
+}
+int main(){
+    int t=0;
+    printf("depois de ja ter compilado uma vez");
+    printf("mais uma pra garatir");
+    scanf("%d", &t);
+    printf("\npegou!");
+    printf("depois de ter debugado uma vez");
+    menu();
+    printf("\n\n\n\nultimavez");
+    return 0;
+}
