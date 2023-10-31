46456b2d07e953b1c457a3bbb320c6b366b4c7df
Marcos Lima <96293424+marcoslima42@users.noreply.github.com>
2023-10-31 16:03:52 -0300

    update q3

--- trabalho5/q3.c
+++ trabalho5/q3.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
-
+#define tam 5
 int main(){
-    
+    int vetor[tam], i=0, pares=0;
+
+    for(i=0; i<tam; i++){
+        printf("Vetor[%d]: ", i);
+        scanf("%d", &vetor[i]);
+    }
+
+    printf("Vetor: ");
+    for(i=0; i<tam; i++){
+        if(pares==0){
+            printf(" %d", vetor[i]);
+            if(i==tam-1){
+                pares=1;
+                i=0;
+            }
+        }
+        else{
+            if(pares==1)
+                printf("\nNUMEROS PARES:");
+            if(vetor[i]%2==0)
+                printf(" %d", vetor[i]);
+            pares++;
+        }
+    }
 }
