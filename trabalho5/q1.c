a55d7ca2541108a5c468a56bb5321b41bc5d3ff4
Marcos Lima <96293424+marcoslima42@users.noreply.github.com>
2023-10-31 14:06:15 -0300

    add q1

---
+++ trabalho5/q1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define tam 2
+
+void percorrerVetor(int x){
+    int a[tam], b[tam], c[tam], i=0, imprimir=1;
+    for(i=0; i<tam; i++){
+        if(x==1){
+            printf("\nVetor A[%d]: ", i+1);
+            scanf("%d", &a[i]);
+        }
+        else if(x==2){
+            printf("\nVetor B[%d]: ", i+1);
+            scanf("%d", &b[i]);
+            c[i]=a[i]+b[i];
+        }
+        else{
+            if(imprimir==1){
+                printf("\nVetor C: ");
+                imprimir=0;
+            }
+            printf(" %d", c[i]);
+        }
+    }
+}
+
+int main(){
+    int x=1;
+    printf("\n\nSOMA DE VETORES");
+    
+    for(int i=0; i<3; i++){
+        percorrerVetor(x);
+        x++;
+    }
+    return 0;
+}
