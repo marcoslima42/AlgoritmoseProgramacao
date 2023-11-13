93bf3860672337b7e000506fbaf6cbe94360fa63
Marcos Lima <96293424+marcoslima42@users.noreply.github.com>
2023-11-13 14:09:28 -0300

    Update q9.c
    removendo o codigo da q9, refatorando o mesmo

--- string/q9.c
+++ string/q9.c
@@ -5,23 +5,17 @@
 #define tam 100
 
 void repetido(char nome[]){
-    int k=0, cont=0;
-    char again[tam];
+    int tamString=0;
 
-    fflush(stdin);
-    for(int i=0; i<tam && nome[i]!='\0'; i++)
-        for(int j=1; j<tam-1 && (cont==0 || cont>2); j++)
-            if(nome[i]==nome[j]){
-                again[k]=nome[i];
-                k++;
-                cont++;
-                if(i==tam-1){
-                    again[k]='\0';
-                }
-            }
+    tamString = strlen(nome);
 
-    printf("%s", again);
-    
+    // "Ana Banana"
+    // resposta: 'n', 'a' 
+
+    for(int i=0; i<=tamString; i++){
+        //n[i]==n[i+1]? printf n[i]
+    }
+
 
 }
 
